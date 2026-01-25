35c163743e958c3f6f6777e6d72f5cd3e029d644
goyallucky <luckygoyal221982@gmail.com>
2026-01-25 16:29:31 +0530

    [Lucky] add implementation to create a Contact in Address Book

---
+++ Contact.cs
@@ -0,0 +1,19 @@
+// Ability to create a Contacts in Address Book with first and last names, address, city, state, zip, phone number and email…
+
+using System;
+
+namespace AddressBookSystem
+{
+    // UC1: Contact Contact model class
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName  { get; set; }
+        public string Address   { get; set; }
+        public string City      { get; set; }
+        public string State     { get; set; }
+        public int    Zip       { get; set; }
+        public string Phone     { get; set; }
+        public string Email     { get; set; }
+    }
+}
