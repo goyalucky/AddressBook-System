5305e449457b5d80d52634fb72ad6e79ae0498df
goyallucky <luckygoyal221982@gmail.com>
2026-01-25 22:52:40 +0530

    [Lucky] add code to add multiple contacts in address book

--- AddressBookMain.cs
+++ AddressBookMain.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Runtime.InteropServices;
 
 namespace AddressBookSystem
 {
@@ -9,40 +8,49 @@ namespace AddressBookSystem
         {
             Console.WriteLine("Welcome to Address Book Program");
 
-            // Create AddressBook object
             AddressBook addressBook = new AddressBook();
+            bool addMore = true;
 
-            // Create Contact object
-            Contact contact = new Contact();
+            // UC5: Add Multiple Contacts
+            while (addMore)
+            {
+                Contact contact = new Contact();
 
-            // UC2: Add New Contact
-            Console.Write("\nEnter First Name: ");
-            contact.FirstName = Console.ReadLine();
+                Console.Write("\nEnter First Name: ");
+                contact.FirstName = Console.ReadLine();
 
-            Console.Write("Enter Last Name: ");
-            contact.LastName = Console.ReadLine();
+                Console.Write("Enter Last Name: ");
+                contact.LastName = Console.ReadLine();
 
-            Console.Write("Enter Address: ");
-            contact.Address = Console.ReadLine();
+                Console.Write("Enter Address: ");
+                contact.Address = Console.ReadLine();
 
-            Console.Write("Enter City: ");
-            contact.City = Console.ReadLine();
+                Console.Write("Enter City: ");
+                contact.City = Console.ReadLine();
 
-            Console.Write("Enter State: ");
-            contact.State = Console.ReadLine();
+                Console.Write("Enter State: ");
+                contact.State = Console.ReadLine();
 
-            Console.Write("Enter Zip: ");
-            contact.Zip = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter Zip: ");
+                contact.Zip = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Enter Phone Number: ");
-            contact.Phone = Console.ReadLine();
+                Console.Write("Enter Phone Number: ");
+                contact.Phone = Console.ReadLine();
 
-            Console.Write("Enter Email: ");
-            contact.Email = Console.ReadLine();
+                Console.Write("Enter Email: ");
+                contact.Email = Console.ReadLine();
 
-            addressBook.AddContact(contact);
+                addressBook.AddContact(contact);
+                Console.WriteLine("Contact added successfully ✔");
 
-            // UC3: Edit Existing Contact 
+                Console.Write("\nDo you want to add another contact? (Y/N): ");
+                string choice = Console.ReadLine().ToUpper();
+
+                if (choice != "Y")
+                    addMore = false;
+            }
+
+            // UC3: Edit Contact
             Console.Write("\nEnter First Name to Edit: ");
             string editFirstName = Console.ReadLine();
 
@@ -51,7 +59,7 @@ namespace AddressBookSystem
 
             addressBook.EditContact(editFirstName, editLastName);
 
-            //UC4: Delete Contact 
+            // UC4: Delete Contact
             Console.Write("\nEnter First Name to Delete: ");
             string deleteFirstName = Console.ReadLine();
 
@@ -59,6 +67,8 @@ namespace AddressBookSystem
             string deleteLastName = Console.ReadLine();
 
             addressBook.DeleteContact(deleteFirstName, deleteLastName);
+
+            Console.WriteLine("\nThank you for using Address Book!");
         }
     }
 }
