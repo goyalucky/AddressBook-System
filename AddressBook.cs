36e9faa86800e3200fb29019392764566526ec34
goyallucky <luckygoyal221982@gmail.com>
2026-01-25 18:55:27 +0530

    [Lucky] add implementation to edit existing contact person using their name

--- AddressBook.cs
+++ AddressBook.cs
@@ -1,4 +1,4 @@
-// UC-2:  Ability to add a new Contact to Address Book
+// UC3: Ability to edit existing contact person using their name
 
 using System;
 using System.Collections.Generic;
@@ -7,14 +7,44 @@ namespace AddressBookSystem
 {
     public class AddressBook
     {
-        // Collection to store contacts
         private List<Contact> contacts = new List<Contact>();
 
-        // Method to add a new contact
+        // UC2: Add new contact
         public void AddContact(Contact contact)
         {
             contacts.Add(contact);
-            Console.WriteLine("Contact added successfully to Address Book.");
+        }
+
+        // UC3: Edit existing contact using name
+        public void EditContact(string firstName, string lastName)
+        {
+            foreach (Contact contact in contacts)
+            {
+                if (contact.FirstName == firstName && contact.LastName == lastName)
+                {
+                    Console.Write("Enter new Address: ");
+                    contact.Address = Console.ReadLine();
+
+                    Console.Write("Enter new City: ");
+                    contact.City = Console.ReadLine();
+
+                    Console.Write("Enter new State: ");
+                    contact.State = Console.ReadLine();
+
+                    Console.Write("Enter new Zip: ");
+                    contact.Zip = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Enter new Phone: ");
+                    contact.Phone = Console.ReadLine();
+
+                    Console.Write("Enter new Email: ");
+                    contact.Email = Console.ReadLine();
+
+                    Console.WriteLine("Contact updated successfully.");
+                    return;
+                }
+            }
+            Console.WriteLine("Contact not found.");
         }
     }
 }
